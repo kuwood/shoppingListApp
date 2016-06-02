6e45185cd68e07a9b4455146a9a9fd2bb80a5294
Keith Underwood <kuwood@users.noreply.github.com>
2016-06-01 20:28:32 -0600

    Add remove item feature

--- js/app.js
+++ js/app.js
@@ -1,3 +1,15 @@
+deleteme = function (ul) {
+  $(ul).on('mouseenter','li', function() {
+    $(this).append('<i class="fa fa-times" aria-hidden="true"></i>')
+    $('i').css('color','red')
+    $('i').click(function() {
+      $(this).parent().remove()
+    })
+  })
+  .on('mouseleave','li', function() {
+    $('i').remove()
+})};
+
 $(document).ready(function() {
   //new item feature
   $('#submit').click(function() {
@@ -6,14 +18,19 @@ $(document).ready(function() {
     /**$('.todoQuantity').prepend('<li> '
      + $('#quantity').val() + '</li>')**/
   })
+  //remove item feature
+  deleteme('.complete')
+  deleteme('.todo')
   //mark complete feature
   $('.todo').on('click','li', function(event) {
     ($('input', this).prop('checked', true));
+    $('i').remove()
     $('.complete').prepend($(this))
   })
   //undo complete feature
   $('.complete').on('click','li', function(event) {
     ($('input', this).prop('checked', false));
+    $('i').remove()
     $('.todo').prepend($(this))
   })
   //clear feature
