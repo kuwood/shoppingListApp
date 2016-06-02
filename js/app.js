5ecbfa609d59b1934fec06364ac34ecad4798eab
Keith Underwood <kuwood@users.noreply.github.com>
2016-06-01 18:58:37 -0600

    change title size, remove quantity feature, add new item, mark complete, complete undo, and clear feature

--- js/app.js
+++ js/app.js
@@ -1,8 +1,23 @@
 $(document).ready(function() {
+  //new item feature
   $('#submit').click(function() {
     $('.todo').prepend('<li><input type="checkbox"> '
      + $('#itemname').val() + '</li>')
-    $('.todoQuantity').prepend('<li> '
-     + $('#quantity').val() + '</li>')
+    /**$('.todoQuantity').prepend('<li> '
+     + $('#quantity').val() + '</li>')**/
+  })
+  //mark complete feature
+  $('.todo').on('click','li', function(event) {
+    ($('input', this).prop('checked', true));
+    $('.complete').prepend($(this))
+  })
+  //undo complete feature
+  $('.complete').on('click','li', function(event) {
+    ($('input', this).prop('checked', false));
+    $('.todo').prepend($(this))
+  })
+  //clear feature
+  $('#clear').click(function() {
+    $('ul > li').remove()
   })
 });
