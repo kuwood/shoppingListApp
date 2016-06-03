9d1312875867f258730855868cb52d07bf40bdf5
Keith Underwood <kuwood@users.noreply.github.com>
2016-06-02 19:25:04 -0600

    rework itemlist layout, add/remove, hide/show and add back in quantity feature.

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 //remove item feature
 deleteme = function (ul) {
   $(ul).on('mouseenter','li', function() {
-    $(this).append('<i class="fa fa-times" aria-hidden="true"></i>')
+    $(this).find('.remove').append('<i class="fa fa-times" aria-hidden="true"></i>')
     $('i').css('color','red')
     $('i').click(function() {
-      $(this).parent().remove()
+      $(this).parent().parent().remove()
     })
   })
   .on('mouseleave','li', function() {
@@ -21,16 +21,24 @@ showhidebutton = function() {
 $(document).ready(function() {
   //new item feature
   $('#submit').click(function() {
-    $('.todo').prepend('<li><input type="checkbox"> '
-     + $('#itemname').val() + '</li>')
-    /**$('.todoQuantity').prepend('<li> '
-     + $('#quantity').val() + '</li>')**/
+    $('.todo').prepend(
+      '<li><div class="divcheck"><input type="checkbox"> '
+     + $('#itemname').val() +
+     '</div><div class="quantity">' +
+     $('#quantity').val() +
+     '</div><div class="remove"></div></li>')
     showhidebutton()
   })
 
   //show/hide todo list feature
   $('.todoContent').on('click','button', function(event) {
-    $('.todo').hide()
+    if ($('.showhide').text() == 'Hide list') {
+      $('.todo').hide()
+      $('.showhide').text('Show list')
+    } else {
+      $('.todo').show()
+      $('.showhide').text('Hide list')
+    }
   })
 
   //remove item feature
